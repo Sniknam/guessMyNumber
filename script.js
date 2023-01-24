8222e39dd15c380ce2d704691e212df60f9ad4ad
Siamak <siamak@Siamaks-MacBook-Pro.local>
2023-01-24 08:10:17 -0800

    adding js to check btn

--- script.js
+++ script.js
@@ -1,1 +1,20 @@
-let massage = document.querySelector('.massage');
+/* document.querySelector('.num').textContent;
+document.querySelector('.score').textContent;
+
+ 
+ document.querySelector('.massage').value;
+  */
+
+ document.querySelector('.check').addEventListener('click', function(){
+
+ const guess=document.querySelector('.massage').value;
+
+if(!guess){
+ document.querySelector('.massage').placeholder=' NO NUMBER';
+
+}
+
+
+ })
+ 
+

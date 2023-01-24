17ae2d7493464a144868a42e36526aa86a2f8945
Siamak <siamak@Siamaks-MacBook-Pro.local>
2023-01-24 10:17:45 -0800

    complete the chesk button

--- script.js
+++ script.js
@@ -1,20 +1,22 @@
 /* document.querySelector('.num').textContent;
 document.querySelector('.score').textContent;
-
- 
- document.querySelector('.massage').value;
+document.querySelector('.massage').value;
   */
 
- document.querySelector('.check').addEventListener('click', function(){
+const secretNum = Math.trunc(Math.random() * 20 + 1);
+document.querySelector(".num").textContent = secretNum;
 
- const guess=document.querySelector('.massage').value;
+document.querySelector(".check").addEventListener("click", function () {
+  const guess = Number(document.querySelector(".inputNum").value);
+  console.log(guess);
 
-if(!guess){
- document.querySelector('.massage').placeholder=' NO NUMBER';
-
-}
-
-
- })
- 
-
+  if (!guess) {
+    document.querySelector(".massage").textContent = " NO NUMBER";
+  } else if (guess === secretNum) {
+    document.querySelector(".massage").textContent = "Correct Number";
+  } else if (guess < secretNum) {
+    document.querySelector(".massage").textContent = "Too Low";
+  } else if (guess > secretNum) {
+    document.querySelector(".massage").textContent = "Too High";
+  }
+});
